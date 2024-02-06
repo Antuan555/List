a10ef8154bb75d62bd66cf4985e073271a785d00
Antuan555 <antuan55678900@gmail.com>
2024-02-06 04:42:27 +0300

    Final test commit

---
+++ Js/Index.js
@@ -0,0 +1,25 @@
+import readlineSync from 'readline-sync';
+// import {body} from '../bin/games/brain-gcd.js'
+export const start = () => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  return name;
+};
+export const finish = (an, corAn, name = start()) => {
+  const result = an;
+  const answer = readlineSync.question('Your answer: ');
+  if (answer === corAn) {
+    console.log('Correct!');
+  } else {
+    console.log(`'${answer}' is wrong answer ;(. Correct answer was '${corAn}'.`);
+    console.log(`Let's try again, ${name}!`);
+    return false;
+  }
+  if (result === 2) {
+    console.log(`Congratulations, ${name}!`);
+  }
+  return result;
+};
+
+// start()
---
+++ makefile
@@ -0,0 +1,8 @@
+test:
+	node Js/Index.js
+install:
+	npm ci
+lint:
+	npx eslint .
+lint-fix:
+	npx eslint . --fix
